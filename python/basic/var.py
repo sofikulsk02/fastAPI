d3a1eeee9f539db2348c3f8c331131dcb6cc120b
SOFIKUL SK <148935407+sofikulsk02@users.noreply.github.com>
2026-08-18 16:01:33 +0530

    a different enviorment

--- python/basic/var.py
+++ python/basic/var.py
@@ -1,7 +1,32 @@
-print("This is just the start")
+from fastapi import FastAPI, status ,HTTPException
+from random import randrange
+from pydantic import BaseModel
+app=FastAPI()
 
-name = "harsh"
+class Post(BaseModel):
+    name:str
+    title:str
+    published:bool=True
+    
 
-print(name)
 
-message="This is a test message"
+my_posts=[{"title":"the oddessy","actor":"mat damon","id":1},{"title":"the prestige","actor":"nolan","id":2}]
+
+@app.get("/")
+def get_data():
+    return {"hey there"}
+
+@app.post("/create_post")
+def createUserPost():
+    return {"This is the new start"}
+    
+    
+        
+@app.post("/post")
+async def createPost(new_post:Post): 
+    if not new_post:
+        return HTTPException(status.HTTP_404_NOT_FOUND,{"message":"No post some error"})
+    my_posts.append(new_post)
+    return {"message": f"title{my_posts}" }
+
+
